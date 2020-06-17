fc9dc4cefcaf6dd8351f2278817c5d584fe7c9ae
Chenruii <33459726+Chenruii@users.noreply.github.com>
2020-06-17 12:13:29 +0200

    Update website_slide_home.php

--- website/plugins/website/widgets/website_slide_home.php
+++ website/plugins/website/widgets/website_slide_home.php
@@ -21,42 +21,22 @@ $links = siteGetTopMenu($MIB_PAGE['lang']);
 <div id="home" class="slider">
             <ul class="slides">
                 <li>
-                    <img src="img/homebenner.jpg"> <!-- random image -->
+                    <img src="../theme/website/img/homebenner1.png"> <!-- random image -->
                     <div class="caption center-align">
                         <div class="single_home">
-                            <h1>ADVENTURER</h1>
-                            <p>MURI, A TEMPLATE AT THE HEART OF THE BEAUTY</p>
-                            <button type="button" class="btn btn-danger m-t-3 waves-effect waves-red">See More</button>
+                            <h1>Solidarité</h1>
+                            <p>To fish peach or not to peach fish</p>
+                            <button type="button" class="btn btn-danger m-t-3 waves-effect waves-red"><a href="annonce"></a>Lire plus</button>
                         </div>
                     </div>
                 </li>
                 <li>
-                    <img src="img/homebenner.jpg"> <!-- random image -->
+                    <img src="../theme/website/img/homebenner.jpg"> <!-- random image -->
                     <div class="caption center-align">
                         <div class="single_home">
-                            <h1>ADVENTURER</h1>
-                            <p>MURI, A TEMPLATE AT THE HEART OF THE BEAUTY</p>
-                            <button type="button" class="btn btn-danger m-t-3 waves-effect waves-red">See More</button>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <img src="img/homebenner.jpg"> <!-- random image -->
-                    <div class="caption center-align">
-                        <div class="single_home">
-                            <h1>ADVENTURER</h1>
-                            <p>MURI, A TEMPLATE AT THE HEART OF THE BEAUTY</p>
-                            <button type="button" class="btn btn-danger m-t-3 waves-effect waves-red">See More</button>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <img src="img/homebenner.jpg"> <!-- random image -->
-                    <div class="caption center-align">
-                        <div class="single_home">
-                            <h1>ADVENTURER</h1>
-                            <p>MURI, A TEMPLATE AT THE HEART OF THE BEAUTY</p>
-                            <button type="button" class="btn btn-danger m-t-3 waves-effect waves-red">See More</button>
+                           <h1>Solidarité</h1>
+                            <p>To fish peach or not to peach fish</p>
+                            <button type="button" class="btn btn-danger m-t-3 waves-effect waves-red">Lire plus</button>
                         </div>
                     </div>
                 </li>
